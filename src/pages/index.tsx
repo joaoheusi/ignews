0e96791e4fed0f9b92b3d188aaf839fd682fdd76
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2022-01-15 20:31:36 -0300

    estrutura da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>IgNews | Home</title>
+      </Head>
+      <h1>
+        Hello <span>Lalalal</span> Next
+      </h1>
+    </>
+  );
+}
